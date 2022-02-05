f0b0ec5aa0f19b533a1f2929cb2e3daf0afde400
Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
2022-02-05 21:35:23 +0200

    multiple commits cause im lazy asf
    turn ad-hoc error into warning, implement basic support for
    xcover3ve, other small bugs and fixes

---
+++ board/samsung/xcover3ve/xcover3ve.c
@@ -0,0 +1,15 @@
+/*
+ * Xcover3ve board file
+ * Copyright (C) 2022, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int exynos_init(void)
+{
+	return 0;
+}
